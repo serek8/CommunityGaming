88a30d1850a3bb49c09329f2e6818ff14c5f5485
lbaran95@gmail.com <Dzidzia1995>
2015-10-31 15:23:50 +0100

    Initial commit for first Unity game, Counter Strike Mini

---
+++ CounterStrikeMini/Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotation : MonoBehaviour {
+
+   
+    private Camera camera;
+    private float speed;
+
+    private Vector3 mousePosition;
+    private Vector3 direction;
+    private float distanceFromObject;
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    void FixedUpdate()
+    {
+
+       
+
+            //Grab the current mouse position on the screen
+            mousePosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - camera.transform.position.z));
+
+            //Rotates toward the mouse
+            GetComponent<Rigidbody>().transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((mousePosition.y - transform.position.y), (mousePosition.x - transform.position.x)) * Mathf.Rad2Deg - 90);
+
+            //Judge the distance from the object and the mouse
+            distanceFromObject = (Input.mousePosition - camera.WorldToScreenPoint(transform.position)).magnitude;
+
+            //Move towards the mouse
+            GetComponent<Rigidbody>().AddForce(direction * speed * distanceFromObject * Time.deltaTime);
+
+      
+
+    }
+}
