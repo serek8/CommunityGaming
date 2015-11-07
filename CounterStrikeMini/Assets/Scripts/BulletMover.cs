8f10701696dd6dd981ac7b8766d2f7e11f4d63fd
Piotr <detoses@github.com>
2015-11-07 13:42:39 +0100

    added temporary bullet

---
+++ CounterStrikeMini/Assets/Scripts/BulletMover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMover : MonoBehaviour {
+
+
+    public float speed;
+
+	void Start () {
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = transform.forward * speed;
+	}
+	
+
+}
