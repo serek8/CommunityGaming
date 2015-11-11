7d04e991623e2a3f9cc440744681dc0d8505fcbf
lbaran95@gmail.com <Dzidzia1995>
2015-11-11 15:55:11 +0100

    Ruch w unity 0-360, dodana obsluga wysylania przez klienta -1

--- CounterStrikeMini/Assets/Scripts/UnityCommunicationUnit.cs
+++ CounterStrikeMini/Assets/Scripts/UnityCommunicationUnit.cs
@@ -22,12 +22,16 @@ public class UnityCommunicationUnit : MonoBehaviour, ISocketAccessibility, IBase
     public float nextFire; // set to 0 in Unity
     public float fireRate; // how often they should shot 
 
+
     // Use this for initialization
     void Start () {
         testCube = Resources.Load("Cube") as GameObject;
        player=  Instantiate(testCube, new Vector3(testCube.transform.position.x + UnityEngine.Random.Range(5.0f, 7.0f), testCube.transform.position.y + UnityEngine.Random.Range(3.0f, 5.0f), 0), Quaternion.identity)as GameObject ;
-        playerTransform = player.GetComponent<Transform>();
+//		player.GetComponent<SpriteRenderer>().material.color = Color(UnityEngine.Random.Range (0.0, 1.0), UnityEngine.Random.Range (0.0, 1.0), UnityEngine.Random.Range (0.0, 1.0));
+		player.GetComponent<SpriteRenderer>().color = new Color(UnityEngine.Random.Range(0.0f,1.0f),UnityEngine.Random.Range(0.0f,1.0f),UnityEngine.Random.Range(0.0f,1.0f));
+			playerTransform = player.GetComponent<Transform>();
         camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+		bullet = Resources.Load ("Bullet") as GameObject;
     }
 
     public void setWarrior(WarriorCommunicationUnit w)
@@ -40,24 +44,29 @@ public class UnityCommunicationUnit : MonoBehaviour, ISocketAccessibility, IBase
     // Update is called once per frame
     void Update () {
 
-        switch (warrior.movement)
-        {
-            case 0:
-                moveUp(playerTransform);
-                break;
-            case 1:
-                moveDown(playerTransform);
-                break;
-            case 2:
-                moveLeft(playerTransform);
-                break;
-            case 3:
-                moveRight(playerTransform);
-                break;
-            default:
-                break;
-        }
-        RotateObject(warrior.rotation);
+//        switch (warrior.movement)
+//        {
+//            case 0:
+//                moveUp(playerTransform);
+//                break;
+//            case 1:
+//                moveDown(playerTransform);
+//                break;
+//            case 2:
+//                moveLeft(playerTransform);
+//                break;
+//            case 3:
+//                moveRight(playerTransform);
+//                break;
+//            default:
+//                break;
+//        }
+		if (warrior.movement != -1) {
+			movePlayer ();
+		}
+		if (warrior.rotation != -1) {
+			RotateObject (warrior.rotation);
+		}
         ShotBullet();
 
     }
@@ -76,32 +85,38 @@ public class UnityCommunicationUnit : MonoBehaviour, ISocketAccessibility, IBase
 
     }
 
+	void movePlayer()
+	{
+		var move = new Vector3 (Mathf.Cos ((2.0f * Mathf.PI * warrior.movement) / 360f), Mathf.Sin ((2.0f * Mathf.PI * warrior.movement) / 360f), 0f);
+		playerTransform.position += move * 3.0f * Time.deltaTime;
+	}
 
-    void moveUp(Transform player)
-    {
-        var move = new Vector3(0f, 0.5f, 0f);
-        player.position += move *3.0f * Time.deltaTime;
 
-    }
-    void moveDown(Transform player)
-    {
-        var move = new Vector3(0f, -0.5f, 0f);
-        player.position += move * 3.0f * Time.deltaTime;
-
-    }
-    void moveLeft(Transform player)
-    {
-        var move = new Vector3(-0.5f, 0f, 0f);
-        player.position += move * 3.0f * Time.deltaTime;
-
-    }
-    void moveRight(Transform player)
-    {
-        var move = new Vector3(0.5f, 0f, 0f);
-        player.position += move * 3.0f * Time.deltaTime;
-        
-
-    }
+//    void moveUp(Transform player)
+//    {
+//        var move = new Vector3(0f, 0.5f, 0f);
+//        player.position += move *3.0f * Time.deltaTime;
+//
+//    }
+//    void moveDown(Transform player)
+//    {
+//        var move = new Vector3(0f, -0.5f, 0f);
+//        player.position += move * 3.0f * Time.deltaTime;
+//
+//    }
+//    void moveLeft(Transform player)
+//    {
+//        var move = new Vector3(-0.5f, 0f, 0f);
+//        player.position += move * 3.0f * Time.deltaTime;
+//
+//    }
+//    void moveRight(Transform player)
+//    {
+//        var move = new Vector3(0.5f, 0f, 0f);
+//        player.position += move * 3.0f * Time.deltaTime;
+//        
+//
+//    }
 
     void RotateObject(int x)
     {
