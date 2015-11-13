e59d4ffa7fbd13801ca91c659d68d60f23ce89ee
Piotr <detoses@github.com>
2015-11-13 22:32:14 +0100

    code cleanup at UnityCommunicationUnit. added PlayerInfo class to hold info about player game status. Added method to control hp of player.

---
+++ CounterStrikeMini/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CommunityGaming {
+
+    public class PlayerInfo {
+
+        public int healthPoints;
+        public int killStreak;
+
+        public PlayerInfo() {
+            ResetStats();
+        }
+        
+        public void ResetStats() {
+            healthPoints = 5;
+            killStreak = 0;
+        }
+    }
+
+}
+
--- CounterStrikeMini/Assets/Scripts/UnityCommunicationUnit.cs
+++ CounterStrikeMini/Assets/Scripts/UnityCommunicationUnit.cs
@@ -4,14 +4,15 @@ using CommunityGaming;
 using System;
 using System.Net.Sockets;
 
-public class UnityCommunicationUnit : MonoBehaviour, ISocketAccessibility, IBaseSerializer
-{
+public class UnityCommunicationUnit : MonoBehaviour, ISocketAccessibility, IBaseSerializer {
 
-   public  WarriorCommunicationUnit warrior;
+    #region UnityCommunicationUnit variables
+    public WarriorCommunicationUnit warrior;
     float speed = 20.0f;
     public GameObject testCube;
     private GameObject player;
     private Transform playerTransform;
+    private float respawnTime;
 
     private Camera camera;
     private Vector3 mousePosition;
@@ -22,14 +23,16 @@ public class UnityCommunicationUnit : MonoBehaviour, ISocketAccessibility, IBase
     public float nextFire; // set to 0 in Unity
     public float fireRate; // how often they should shot 
 
+    private PlayerInfo playerInfo = new PlayerInfo();
+    #endregion
+
 
     // Use this for initialization
     void Start () {
         testCube = Resources.Load("Cube") as GameObject;
-       player=  Instantiate(testCube, new Vector3(testCube.transform.position.x + UnityEngine.Random.Range(5.0f, 7.0f), testCube.transform.position.y + UnityEngine.Random.Range(3.0f, 5.0f), 0), Quaternion.identity)as GameObject ;
-//		player.GetComponent<SpriteRenderer>().material.color = Color(UnityEngine.Random.Range (0.0, 1.0), UnityEngine.Random.Range (0.0, 1.0), UnityEngine.Random.Range (0.0, 1.0));
+        player = Instantiate(testCube, new Vector3(testCube.transform.position.x + UnityEngine.Random.Range(5.0f, 7.0f), testCube.transform.position.y + UnityEngine.Random.Range(3.0f, 5.0f), 0), Quaternion.identity)as GameObject ;
 		player.GetComponent<SpriteRenderer>().color = new Color(UnityEngine.Random.Range(0.0f,1.0f),UnityEngine.Random.Range(0.0f,1.0f),UnityEngine.Random.Range(0.0f,1.0f));
-			playerTransform = player.GetComponent<Transform>();
+		playerTransform = player.GetComponent<Transform>();
         playerTransform.SetParent(GameObject.Find("Players").GetComponent<Transform>());
         camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 		bullet = Resources.Load ("Bullet") as GameObject;
@@ -40,28 +43,9 @@ public class UnityCommunicationUnit : MonoBehaviour, ISocketAccessibility, IBase
         warrior = w;
         
     }
- 
 
     // Update is called once per frame
     void Update () {
-
-//        switch (warrior.movement)
-//        {
-//            case 0:
-//                moveUp(playerTransform);
-//                break;
-//            case 1:
-//                moveDown(playerTransform);
-//                break;
-//            case 2:
-//                moveLeft(playerTransform);
-//                break;
-//            case 3:
-//                moveRight(playerTransform);
-//                break;
-//            default:
-//                break;
-//        }
 		if (warrior.movement != -1) {
 			movePlayer ();
 		}
@@ -69,55 +53,14 @@ public class UnityCommunicationUnit : MonoBehaviour, ISocketAccessibility, IBase
 			RotateObject (warrior.rotation);
 		}
         ShotBullet();
-
     }
 
-   
-
-    public void didDisconnect()
-    {
-        throw new NotImplementedException();
-    }
-
-   public UnityCommunicationUnit(NetworkStream stream)
-    {
-      
-        
-
-    }
-
-	void movePlayer()
+    #region Player control methods
+    void movePlayer()
 	{
 		var move = new Vector3 (Mathf.Cos ((2.0f * Mathf.PI * warrior.movement) / 360f), Mathf.Sin ((2.0f * Mathf.PI * warrior.movement) / 360f), 0f);
 		playerTransform.position += move * 3.0f * Time.deltaTime;
 	}
-
-
-//    void moveUp(Transform player)
-//    {
-//        var move = new Vector3(0f, 0.5f, 0f);
-//        player.position += move *3.0f * Time.deltaTime;
-//
-//    }
-//    void moveDown(Transform player)
-//    {
-//        var move = new Vector3(0f, -0.5f, 0f);
-//        player.position += move * 3.0f * Time.deltaTime;
-//
-//    }
-//    void moveLeft(Transform player)
-//    {
-//        var move = new Vector3(-0.5f, 0f, 0f);
-//        player.position += move * 3.0f * Time.deltaTime;
-//
-//    }
-//    void moveRight(Transform player)
-//    {
-//        var move = new Vector3(0.5f, 0f, 0f);
-//        player.position += move * 3.0f * Time.deltaTime;
-//        
-//
-//    }
 
     void RotateObject(int x)
     {
@@ -135,15 +78,37 @@ public class UnityCommunicationUnit : MonoBehaviour, ISocketAccessibility, IBase
         }
     }
 
+    void PlayerGotHit() {
+        playerInfo.healthPoints--;
+        if(playerInfo.healthPoints == 0) {
+            DestroyObject(player);
+            playerInfo.ResetStats();
+            StartCoroutine(WaitForRespawn());
+        }
+    }
 
+    IEnumerator WaitForRespawn() {
+        yield return new WaitForSeconds(respawnTime);
+    }
 
-    public void readFromStream()
-    {
+    #endregion
+
+    #region Serverside methods
+    public void didDisconnect() {
+        throw new NotImplementedException();
+    }
+
+    public UnityCommunicationUnit(NetworkStream stream) {
+
+    }
+
+    public void readFromStream() {
         ((IBaseSerializer)warrior).readFromStream();
     }
 
-    public void writeFromStream()
-    {
+    public void writeFromStream() {
         ((IBaseSerializer)warrior).writeFromStream();
     }
+    #endregion
+
 }
