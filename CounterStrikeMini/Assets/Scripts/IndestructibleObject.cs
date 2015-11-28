2de2ab760d4bafd67476265f0404ec8a3f58511c
Piotr <detoses@github.com>
2015-11-28 14:22:15 +0100

    Added indestructibleObject  and MapController class, changed destroyable objects hp to 10, changes color when got hit by bullet. Added few objects for map

---
+++ CounterStrikeMini/Assets/Scripts/IndestructibleObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CommunityGaming {
+
+    public class IndestructibleObject : MonoBehaviour {
+
+        void OnTriggerEnter2D(Collider2D other) {
+            if (other.gameObject.tag == "Bullet") {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
---
+++ CounterStrikeMini/Assets/Scripts/MapController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CommunityGaming {
+
+    public class MapController : MonoBehaviour {
+
+        public IndestructibleObject[] indestructibleObjects;
+        public DestroyableObject[] destroyableObjects;
+
+    }
+}
